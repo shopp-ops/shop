75cdf1791094fbe778200c08dde4f17e4742a090
aleksaqm <perovic.aleksa03@gmail.com>
2026-06-18 15:44:05 +0200

    refactor(api): use order repository abstraction in order service

--- api/src/orders/orders.module.ts
+++ api/src/orders/orders.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { isStandardDatabaseDriver } from '../database/database-driver';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { MongoOrdersRepository } from './repositories/mongo-orders.repository';
+import { OrdersRepository } from './repositories/orders.repository';
+import { PostgresOrdersRepository } from './repositories/postgres-orders.repository';
+
+const useStandardDatabase = isStandardDatabaseDriver();
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: useStandardDatabase
+    ? [TypeOrmModule.forFeature([Order, OrderItem])]
+    : [],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [
+    OrdersService,
+    useStandardDatabase
+      ? {
+          provide: OrdersRepository,
+          useClass: PostgresOrdersRepository,
+        }
+      : {
+          provide: OrdersRepository,
+          useClass: MongoOrdersRepository,
+        },
+  ],
 })
 export class OrdersModule {}
